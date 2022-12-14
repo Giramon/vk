32f8ac2e88e7dcaf62d4a3da89994e5f2fef7e6c
Giramon <120495791+Giramon@users.noreply.github.com>
2022-12-14 17:59:19 +0500

    sait

---
+++ .github/workflows/3.js
@@ -0,0 +1,34 @@
+ymaps.ready(init);
+
+function init() {
+    var map = new ymaps.Map('map', {
+        center: [55.1553, 61.4313],
+        zoom: 12,
+        controls: ['zoomControl'],
+        behaviors: ['drag']
+    });
+    var geolocation = ymaps.geolocation;
+ 
+    geolocation.get({
+    
+        provider: 'yandex',
+        
+        mapStateAutoApply: true,
+        
+        autoReverseGeocode: true
+    }).then(function (result) {
+ 
+        
+        myMap.geoObjects.add(result.geoObjects);
+ 
+        var address = result.geoObjects.get(0).properties.get('text');
+        document.getElementById('address').innerText = address;
+ 
+        
+        console.log(result.geoObjects.get(0).properties.get('metaDataProperty'));
+ 
+    });
+   
+    }
+
+    
