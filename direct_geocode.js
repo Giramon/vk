73fd8a1ff5a8998a1c2b3a241e606abafcaa4ed4
Giramon <120495791+Giramon@users.noreply.github.com>
2022-12-17 21:05:29 +0500

    Add files via upload

---
+++ direct_geocode.js
@@ -0,0 +1,7 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.15, 61.43],
+            zoom: 12,
+            controls: ['geolocationControl']
+        });
+});
